ec726ab3be3a01b125c640fd514f75438e5a6145
Mohammed Aly <maktapnoor@gmail.com>
2026-03-14 16:48:06 +0200

    Stopped at sorting

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,41 @@
+ 
+ import Product from  '../models/product.js'
+
+ // just for testing
  const getAllProductsStatic = async (req, res) => {
-    res.status(200).json({ msg: 'products testing route' });
+    const search = 'uto';
+    const products = await Product.find({
+        name : {$regex: search, $options: 'i'}
+    });
+    res.status(200).json({nbHits: products.length, products});
 };
 
  const getAllProducts = async (req, res) => {
-    res.status(200).json({ msg: 'products route' });
+
+    const {featured, company, name} = req.query;
+
+    const queryObject =  {};
+    
+        if (featured) {
+            queryObject.featured  = featured === 'true' ? true : false;
+            console.log(queryObject)
+        }
+        
+        if (company) {
+            queryObject.company = company;
+        }
+
+        if (name) {
+            queryObject.name = {$regex: name, $options: 'i'}
+        }
+        
+    console.log(queryObject)
+
+    const products = await Product.find(queryObject);
+
+    res.status(200).json({nbHits: products.length, products});
+    
+
 };
 
 export {
